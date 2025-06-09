7dcf18fbd7bf2668c2f8a390f516f21000c2b2ae
Kaiquan <kaiquanli@KaiquandeMacBook-Air.local>
2025-06-09 10:09:28 +0800

    feat: 主色调整

--- frontend/wiggly-flow/ui/separator/index.tsx
+++ frontend/wiggly-flow/ui/separator/index.tsx
@@ -15,7 +15,7 @@ export const Separator: FC<SeparatorProps> = ({
 }) => (
   <Primitive.Root
     className={clsx(
-      "bg-primary-disabled",
+      "bg-disabled",
       "data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full",
       "data-[orientation=vertical]:w-px data-[orientation=vertical]:h-full",
       "data-[orientation=vertical]:min-h-2",
