f1eea5b097afdd96285e6692b60e9ca7b92cb253
Kaiquan <kaiquanli@KaiquandeMacBook-Air.local>
2025-06-16 16:48:22 +0800

    feat: 优化envstore

--- frontend/wiggly-flow/lib/store/env-variable-store.ts
+++ frontend/wiggly-flow/lib/store/env-variable-store.ts
@@ -1,4 +1,4 @@
-import { useSyncExternalStore } from "react";
+import { useCallback, useRef, useSyncExternalStore } from "react";
 import { createStore, StateCreator } from "zustand";
 
 interface EnvVariableStoreStateProps {
@@ -10,18 +10,35 @@ const envVariableStoreState: StateCreator<EnvVariableStoreStateProps> = (
   set
 ) => ({
   envVariables: [],
-  setEnvVariables: (envVariables) =>
-    set(() => ({
-      envVariables,
-    })),
+  setEnvVariables: (envVariables) => set(() => ({ envVariables })),
 });
 
 export const EnvVariableStore = createStore(envVariableStoreState);
 
-export const useEnvVariableStore: any = () => {
-  return useSyncExternalStore(
-    EnvVariableStore.subscribe,
-    EnvVariableStore.getState,
-    EnvVariableStore.getState
+export function useEnvVariableStore<T>(
+  selector: (state: EnvVariableStoreStateProps) => T = (state) =>
+    state as unknown as T,
+  equalityFn: (a: T, b: T) => boolean = Object.is
+): T {
+  const lastSelectedRef = useRef<T | undefined>(undefined);
+  const subscribe = useCallback(
+    (callback: () => void) => {
+      return EnvVariableStore.subscribe(() => {
+        const newSelected = selector(EnvVariableStore.getState());
+        if (!equalityFn(lastSelectedRef.current!, newSelected)) {
+          lastSelectedRef.current = newSelected;
+          callback();
+        }
+      });
+    },
+    [selector, equalityFn]
   );
-};
+
+  const getSnapshot = useCallback(() => {
+    const selected = selector(EnvVariableStore.getState());
+    lastSelectedRef.current = selected;
+    return selected;
+  }, [selector]);
+
+  return useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+}
